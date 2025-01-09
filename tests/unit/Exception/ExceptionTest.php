48549d29015d1a893d5f97ec6cddcb1bea34a430
Derek Stephen McLean <delboy1978uk@gmail.com>
2025-01-09 03:13:54 +0100

    test all scennarios

--- tests/unit/Exception/ExceptionTest.php
+++ tests/unit/Exception/ExceptionTest.php
@@ -11,7 +11,7 @@ use function ob_start;
 
 class ExceptionTest extends Unit
 {
-    public function testForm()
+    public function testShutdowError()
     {
         putenv('TEST_ERROR=true');
         $handler = Exception::getShutdownHandler();
@@ -22,6 +22,31 @@ class ExceptionTest extends Unit
         $this->assertIsString($output);
         $this->assertStringContainsString('<body id=\'error\'>', $output);
         putenv('TEST_ERROR=false');
+    }
+
+    public function testNoShutdownError()
+    {
+        $env = getenv('APPLICATION_ENV');
+        putenv('TEST_ERROR=true');
+        putenv('APPLICATION_ENV=production');
+        $handler = Exception::getShutdownHandler();
+        $this->assertInstanceOf(Closure::class, $handler);
+        ob_start();
+        $handler();
+        $output = ob_get_clean();
+        $this->assertStringContainsString('There was an error', $output);
+        putenv('APPLICATION_ENV=' . $env);
+        putenv('TEST_ERROR=false');
+    }
+
+    public function testProductionError()
+    {
+        $handler = Exception::getShutdownHandler();
+        $this->assertInstanceOf(Closure::class, $handler);
+        ob_start();
+        $handler();
+        $output = ob_get_clean();
+        $this->assertEmpty($output);
     }
 }
 
